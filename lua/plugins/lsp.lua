06c6013ec117cc7ac3f5b44cae3ea42f676b1d14
AleksL04 <sasha.likhoshva@gmail.com>
2026-04-14 20:48:25 -0400

    auto format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,194 +1,206 @@
 return {
-    {
-    "nvim-lspconfig",
-    auto_enable = true,
-    -- NOTE: define a function for lsp,
-    -- and it will run for all specs with type(plugin.lsp) == table
-    -- when their filetype trigger loads them
-    lsp = function(plugin)
-      vim.lsp.config(plugin.name, plugin.lsp or {})
-      vim.lsp.enable(plugin.name)
-    end,
-    -- set up our on_attach function once before the spec loads
-    before = function(_)
-      vim.lsp.config('*', {
-        on_attach = function(_, bufnr)
+	{
+		"nvim-lspconfig",
+		auto_enable = true,
+		-- NOTE: define a function for lsp,
+		-- and it will run for all specs with type(plugin.lsp) == table
+		-- when their filetype trigger loads them
+		lsp = function(plugin)
+			vim.lsp.config(plugin.name, plugin.lsp or {})
+			vim.lsp.enable(plugin.name)
+		end,
+		-- set up our on_attach function once before the spec loads
+		before = function(_)
+			vim.lsp.config("*", {
+				on_attach = function(_, bufnr)
+					-- we create a function that lets us more easily define mappings specific
+					-- for LSP related items. It sets the mode, buffer and description for us each time.
+					local nmap = function(keys, func, desc)
+						if desc then
+							desc = "LSP: " .. desc
+						end
+						vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+					end
 
-          -- we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
-          local nmap = function(keys, func, desc)
-            if desc then
-              desc = 'LSP: ' .. desc
-            end
-            vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-          end
+					nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+					nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+					nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+					nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+					nmap("gr", function()
+						Snacks.picker.lsp_references()
+					end, "[G]oto [R]eferences")
+					nmap("gI", function()
+						Snacks.picker.lsp_implementations()
+					end, "[G]oto [I]mplementation")
+					nmap("<leader>ds", function()
+						Snacks.picker.lsp_symbols()
+					end, "[D]ocument [S]ymbols")
+					nmap("<leader>ws", function()
+						Snacks.picker.lsp_workspace_symbols()
+					end, "[W]orkspace [S]ymbols")
 
-          nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-          nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-          nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-          nmap('gr', function() Snacks.picker.lsp_references() end, '[G]oto [R]eferences')
-          nmap('gI', function() Snacks.picker.lsp_implementations() end, '[G]oto [I]mplementation')
-          nmap('<leader>ds', function() Snacks.picker.lsp_symbols() end, '[D]ocument [S]ymbols')
-          nmap('<leader>ws', function() Snacks.picker.lsp_workspace_symbols() end, '[W]orkspace [S]ymbols')
+					-- See `:help K` for why this keymap
+					nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+					nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
-          -- See `:help K` for why this keymap
-          nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-          nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+					-- Lesser used LSP functionality
+					nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+					nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+					nmap("<leader>wl", function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, "[W]orkspace [L]ist Folders")
 
-          -- Lesser used LSP functionality
-          nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-          nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-          nmap('<leader>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, '[W]orkspace [L]ist Folders')
+					-- Create a command `:Format` local to the LSP buffer
+					vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+						vim.lsp.buf.format()
+					end, { desc = "Format current buffer with LSP" })
+				end,
+			})
+		end,
+	},
+	{
+		"mason.nvim",
+		enabled = not nixInfo.isNix,
+		priority = 100, -- <- run lsp hook before lspconfig's hook
+		on_plugin = { "nvim-lspconfig" },
+		lsp = function(plugin)
+			vim.cmd.MasonInstall(plugin.name)
+		end,
+	},
+	{
+		-- lazydev makes your lua lsp load only the relevant definitions for a file.
+		-- It also gives us a nice way to correlate globals we create with files.
+		"lazydev.nvim",
+		auto_enable = true,
+		cmd = { "LazyDev" },
+		ft = "lua",
+		after = function(_)
+			require("lazydev").setup({
+				library = {
+					{ words = { "nixInfo%.lze" }, path = nixInfo("lze", "plugins", "start", "lze") .. "/lua" },
+					{
+						words = { "nixInfo%.lze" },
+						path = nixInfo("lzextras", "plugins", "start", "lzextras") .. "/lua",
+					},
+				},
+			})
+		end,
+	},
+	{
+		-- name of the lsp
+		"lua_ls",
+		for_cat = "lua",
+		-- provide a table containing filetypes,
+		-- and then whatever your functions defined in the function type specs expect.
+		-- in our case, it just expects the normal lspconfig setup options,
+		-- but with a default on_attach and capabilities
+		lsp = {
+			-- if you provide the filetypes it doesn't ask lspconfig for the filetypes
+			-- (meaning it doesn't call the callback function we defined in the main init.lua)
+			filetypes = { "lua" },
+			settings = {
+				Lua = {
+					signatureHelp = { enabled = true },
+					diagnostics = {
+						globals = { "nixInfo", "vim" },
+						disable = { "missing-fields" },
+					},
+				},
+			},
+		},
+		-- also these are regular specs and you can use before and after and all the other normal fields
+	},
+	{
+		"nixd",
+		enabled = nixInfo.isNix, -- mason doesn't have nixd
+		for_cat = "nix",
+		lsp = {
+			filetypes = { "nix" },
+			settings = {
+				nixd = {
+					nixpkgs = {
+						expr = [[import <nixpkgs> {}]],
+					},
+					options = {},
+					formatting = {
+						command = { "nixfmt" },
+					},
+					diagnostic = {
+						suppress = {
+							"sema-escaping-with",
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"conform.nvim",
+		auto_enable = true,
+		-- cmd = { "" },
+		-- event = "",
+		-- ft = "",
+		keys = {
+			{ "<leader>FF", desc = "[F]ormat [F]ile" },
+		},
+		-- colorscheme = "",
+		after = function(plugin)
+			local conform = require("conform")
 
-          -- Create a command `:Format` local to the LSP buffer
-          vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-            vim.lsp.buf.format()
-          end, { desc = 'Format current buffer with LSP' })
-        end
-      })
-    end,
-  },
-  {
-    "mason.nvim",
-    enabled = not nixInfo.isNix,
-    priority = 100, -- <- run lsp hook before lspconfig's hook
-    on_plugin = { "nvim-lspconfig" },
-    lsp = function(plugin)
-      vim.cmd.MasonInstall(plugin.name)
-    end,
-  },
-  {
-    -- lazydev makes your lua lsp load only the relevant definitions for a file.
-    -- It also gives us a nice way to correlate globals we create with files.
-    "lazydev.nvim",
-    auto_enable = true,
-    cmd = { "LazyDev" },
-    ft = "lua",
-    after = function(_)
-      require('lazydev').setup({
-        library = {
-          { words = { "nixInfo%.lze" }, path = nixInfo("lze", "plugins", "start", "lze") .. '/lua', },
-          { words = { "nixInfo%.lze" }, path = nixInfo("lzextras", "plugins", "start", "lzextras") .. '/lua' },
-        },
-      })
-    end,
-  },
-  {
-    -- name of the lsp
-    "lua_ls",
-    for_cat = "lua",
-    -- provide a table containing filetypes,
-    -- and then whatever your functions defined in the function type specs expect.
-    -- in our case, it just expects the normal lspconfig setup options,
-    -- but with a default on_attach and capabilities
-    lsp = {
-      -- if you provide the filetypes it doesn't ask lspconfig for the filetypes
-      -- (meaning it doesn't call the callback function we defined in the main init.lua)
-      filetypes = { 'lua' },
-      settings = {
-        Lua = {
-          signatureHelp = { enabled = true },
-          diagnostics = {
-            globals = { "nixInfo", "vim", },
-            disable = { 'missing-fields' },
-          },
-        },
-      },
-    },
-    -- also these are regular specs and you can use before and after and all the other normal fields
-  },
-  {
-    "nixd",
-    enabled = nixInfo.isNix, -- mason doesn't have nixd
-    for_cat = "nix",
-    lsp = {
-      filetypes = { "nix" },
-      settings = {
-        nixd = {
-          nixpkgs = {
-            expr = [[import <nixpkgs> {}]],
-          },
-          options = {
-          },
-          formatting = {
-            command = { "nixfmt" }
-          },
-          diagnostic = {
-            suppress = {
-              "sema-escaping-with"
-            }
-          }
-        }
-      },
-    },
-  },
-  {
-    "conform.nvim",
-    auto_enable = true,
-    -- cmd = { "" },
-    -- event = "",
-    -- ft = "",
-    keys = {
-      { "<leader>FF", desc = "[F]ormat [F]ile" },
-    },
-    -- colorscheme = "",
-    after = function (plugin)
-      local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					-- NOTE: download some formatters
+					-- and configure them here
+					lua = nixInfo(nil, "settings", "cats", "lua") and { "stylua" } or nil,
+					-- go = { "gofmt", "golint" },
+					-- templ = { "templ" },
+					-- Conform will run multiple formatters sequentially
+					-- python = { "isort", "black" },
+					-- Use a sub-list to run only the first available formatter
+					-- javascript = { { "prettierd", "prettier" } },
+				},
+				format_on_save = {
+					timeout_ms = 1000,
+					lsp_format = true,
+				},
+			})
 
-      conform.setup({
-        formatters_by_ft = {
-          -- NOTE: download some formatters
-          -- and configure them here
-          lua = nixInfo(nil, "settings", "cats", "lua") and { "stylua" } or nil,
-          -- go = { "gofmt", "golint" },
-          -- templ = { "templ" },
-          -- Conform will run multiple formatters sequentially
-          -- python = { "isort", "black" },
-          -- Use a sub-list to run only the first available formatter
-          -- javascript = { { "prettierd", "prettier" } },
-        },
-      })
+			vim.keymap.set({ "n", "v" }, "<leader>FF", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end, { desc = "[F]ormat [F]ile" })
+		end,
+	},
+	{
+		"nvim-lint",
+		auto_enable = true,
+		-- cmd = { "" },
+		event = "FileType",
+		-- ft = "",
+		-- keys = "",
+		-- colorscheme = "",
+		after = function(plugin)
+			require("lint").linters_by_ft = {
+				-- NOTE: download some linters
+				-- and configure them here
+				-- markdown = {'vale',},
+				-- javascript = { 'eslint' },
+				-- typescript = { 'eslint' },
+			}
 
-      vim.keymap.set({ "n", "v" }, "<leader>FF", function()
-        conform.format({
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 1000,
-        })
-      end, { desc = "[F]ormat [F]ile" })
-    end,
-  },
-  {
-    "nvim-lint",
-    auto_enable = true,
-    -- cmd = { "" },
-    event = "FileType",
-    -- ft = "",
-    -- keys = "",
-    -- colorscheme = "",
-    after = function (plugin)
-      require('lint').linters_by_ft = {
-        -- NOTE: download some linters
-        -- and configure them here
-        -- markdown = {'vale',},
-        -- javascript = { 'eslint' },
-        -- typescript = { 'eslint' },
-      }
-
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        callback = function()
-          require("lint").try_lint()
-        end,
-      })
-    end,
-  },
-  {
-    "clangd",
-    lsp = {filetypes = {"c", "cpp", "objc", "objcpp"},
-    },
-  },
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
+		end,
+	},
+	{
+		"clangd",
+		lsp = { filetypes = { "c", "cpp", "objc", "objcpp" } },
+	},
 }
