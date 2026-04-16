98ba9d15e7900be31708864a2dd541609e6cfa0d
AleksL04 <sasha.likhoshva@gmail.com>
2026-04-16 17:12:15 -0400

    dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,22 +2,9 @@ return {
   {
     "nvim-dap",
     auto_enable = true,
-    on_plugin = function()
-      local dap = require('dap')
-      -- Adapter for C++
-      dap.adapters.gdb = {
-        type = "executable",
-        command = "gdb",
-        args = { "-i", "dap" }
-      }
-    end,
   },
   {
     "nvim-dap-view",
     auto_enable = true,
-    after = { "nvim-dap" },
-    on_plugin = function()
-      require("dap-view").setup({})
-    end,
   },
 }
