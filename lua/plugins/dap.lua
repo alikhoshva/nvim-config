21215b2a25285fd7794ca92b486a6d72dddac719
AleksL04 <sasha.likhoshva@gmail.com>
2026-04-16 17:25:32 -0400

    dap2

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,10 +1,18 @@
 return {
-  {
-    "nvim-dap",
-    auto_enable = true,
-  },
-  {
-    "nvim-dap-view",
-    auto_enable = true,
-  },
+	{
+		"nvim-dap",
+		auto_enable = true,
+		after = function(plugin)
+			local dap = require("dap")
+			dap.adapters.gdb = {
+				type = "executable",
+				command = "gdb",
+				args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+			}
+		end,
+	},
+	{
+		"nvim-dap-view",
+		auto_enable = true,
+	},
 }
